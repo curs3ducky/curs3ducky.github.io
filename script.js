7225cdd1329f8a547da3b9730937a09142818c7f
curs3ducky <163217514+curs3ducky@users.noreply.github.com>
2024-03-13 00:14:27 -0700

    Create script.js
    Add basic JavaScript for interaction

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var bubbleArea = document.getElementById('bubble-area');
+    bubbleArea.addEventListener('click', function() {
+        alert('Bubble area clicked!');
+    });
+});
